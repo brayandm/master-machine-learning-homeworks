0b1b385e65a38a7edeb0b9315bcb9ac9b77cd246
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-02 19:41:59 +0100

    Update package dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-
 pip3 install numpy pandas matplotlib scikit-learn scipy seaborn
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
